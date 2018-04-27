9af03e51629a4db5b54f9b5e2afff956828bcdd3
Test <xingyub2@illinois.edu>
2018-04-26 19:14:40 -0500

    try

---
+++ app/src/main/java/com/example/test/cs125/Main2Activity.java
@@ -0,0 +1,24 @@
+package com.example.test.cs125;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        final ImageButton play = findViewById(R.id.Play);
+        /* click listener*/
+        final ImageButton save = findViewById(R.id.Save);
+        final ImageButton share = findViewById(R.id.Share);
+        final ImageButton Return = findViewById(R.id.Return);
+        final ProgressBar progress = findViewById(R.id.progressBar);
+
+    }
+}
---
+++ app/src/main/java/com/example/test/cs125/MainActivity.java
@@ -0,0 +1,62 @@
+package com.example.test.cs125;
+
+import android.app.DownloadManager;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.speech.tts.TextToSpeech;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.gson.JsonObject;
+import java.util.Locale;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+
+public class MainActivity extends AppCompatActivity {
+    /** Request queue for our API requests. */
+    private static RequestQueue requestQueue;
+
+    private EditText write;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final TextInputEditText ed = findViewById(R.id.Input);
+        final TextToSpeech ttobj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                ttobj.setLanguage(Locale.ENGLISH);
+            }
+        });
+        final Button enter = findViewById(R.id.enter);
+        enter.setOnClickListener(new View.OnClickListener() {
+            public void onClick(final View v) {
+                String toSpeak = ed.getText().toString();
+                Toast.makeText(getApplicationContext(), toSpeak, Toast.LENGTH_SHORT).show();
+                enter.setText("clicked");
+                ttobj.speak(toSpeak, TextToSpeech.QUEUE_FLUSH, null);
+            }
+        });
+    }
+
+    void startAPICall() {
+
+    }
+
+}
