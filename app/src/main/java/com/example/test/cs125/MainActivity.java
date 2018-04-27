0d506c67511bd74fadc09e0823318b96bbf8b26d
Test <xingyub2@illinois.edu>
2018-04-27 15:34:39 -0500

    return button

--- app/src/main/java/com/example/test/cs125/MainActivity.java
+++ app/src/main/java/com/example/test/cs125/MainActivity.java
@@ -1,36 +1,15 @@
 package com.example.test.cs125;
 
-import android.app.DownloadManager;
 import android.content.Intent;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
-import android.widget.TextView;
-import android.speech.tts.TextToSpeech;
-import android.widget.EditText;
-import android.widget.Toast;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.design.widget.TextInputEditText;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Button;
-import com.google.gson.JsonObject;
 
 import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity implements Serializable{
-    private EditText write;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
