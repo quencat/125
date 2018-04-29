99eb1bb37adc1efe7c3a6e44c4535bd7fe1269eb
Test <xingyub2@illinois.edu>
2018-04-29 10:59:49 -0500

    3 tones

--- app/src/main/java/com/example/test/cs125/Main2Activity.java
+++ app/src/main/java/com/example/test/cs125/Main2Activity.java
@@ -2,14 +2,11 @@ package com.example.test.cs125;
 
 import android.content.Intent;
 import android.speech.tts.TextToSpeech;
-import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.ProgressBar;
+
 import android.widget.TextView;
 import android.widget.Toast;
 
